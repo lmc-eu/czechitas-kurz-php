5341cebf5883a83cbf1564086dc06a4ebfdee444
Martin Holcman <martin.holcman@lmc.eu>
2019-12-02 11:34:00 +0100

    Reseni ukolu na 9. lekci

---
+++ prihlas-me.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+$uzivatelskeJmeno = '';
+$uzivatelskeHeslo = '';
+
+// pokud neexistuje session, zalozime si ji
+if (!isset($_SESSION['pokusu_prihlaseni'])) {
+    $_SESSION['pokusu_prihlaseni'] = 0;
+}
+
+// pokud se opravdu uzivatel snazi prihlasit, zapocitame mu to a zobrazime hlasku
+if (isset($_GET['jmeno']) && isset($_GET['heslo'])) {
+    $_SESSION['pokusu_prihlaseni']++;
+
+    echo "Toto je ".$_SESSION['pokusu_prihlaseni'].". pokus o přihlášení.";
+
+    $uzivatelskeJmeno = $_GET['jmeno'];
+    $uzivatelskeHeslo = $_GET['heslo'];
+// pokud se nesnazi prihlasit, jenom to rekneme
+} else {
+    echo "Toto není pokus o přihlášení.";
+}
