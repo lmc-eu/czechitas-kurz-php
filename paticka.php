bf93de645adcfc9c6b0ffee83633d5352138fee0
Martin Holcman <martin.holcman@lmc.eu>
2019-12-03 10:33:39 +0100

    Oddělení hlavičky a patičky do vlastních souborů

---
+++ paticka.php
@@ -0,0 +1,13 @@
+        <!-- patička -->
+        <div class="footer navbar navbar-light bg-light rounded">
+            <p class="mb-0">
+                Spolu s Czechitas jsme to s láskou upekli v LMC s.r.o.
+                |
+                Počet zhlédnutí blogu: <?php echo $pocetZhlednuti; ?>
+                |
+                Stránka vygenerována: <?php echo date('j. n. Y, H:i:s'); ?>
+            </p>
+        </div>
+    </div>
+</body>
+</html>
